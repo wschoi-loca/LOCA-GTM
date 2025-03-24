003e26af9a08b9a47767eb2c3a5edb4c7b2f8b4b
wschoi-loca <choiwonseok@lottecard.co.kr>
2025-03-24 16:55:20 +0900

    Update auto-attribute.js

--- auto-attribute.js
+++ auto-attribute.js
@@ -4,18 +4,31 @@
     return element.closest(`[${prefix}]`) !== null;
   }
 
-  // Helper function to get the label value based on specified conditions
-  function getLabelValue(element) {
+
+// Helper function to get the label value based on specified conditions
+function getLabelValue(element) {
     let value = "";
     const svgCloseCheck = element.querySelector('svg[xmlns="http://www.w3.org/2000/svg"] path[d="M21 3 3 21M3 3l18 18"]');
     const svgMoveCheck = element.querySelector('svg[xmlns="http://www.w3.org/2000/svg"] path[d="m9 18 6-6-6-6"]');
 
+    // Function to apply transformations based on provided regex patterns
+    const applyTransformations = (text, patterns) => {
+      if (!text) return "";
+      let transformedText = text;
+      patterns.forEach(pattern => {
+        transformedText = transformedText.replace(new RegExp(pattern, 'g'), '|');
+      });
+      return transformedText;
+    };
+
+    // Define the patterns to be replaced
+    const patterns = ['\\n', '<br\\s*\\/?>', '\\t'];
+
     // 라벨값을 가져오기 위한 조건들
     const conditions = [
-      () => element.innerText,
-      () => element.textContent,
-      () => element.outerText,
-      () => element.title,
+      () => applyTransformations(element.innerText, patterns),
+      () => applyTransformations(element.textContent, patterns),
+      () => applyTransformations(element.outerText, patterns),
       () => {
         // Find the closest parent with innerText, up to 5 levels
         let parent = element.parentElement;
@@ -24,15 +37,14 @@
           parent = parent.parentElement;
           level++;
         }
-        return parent.innerText ? parent.innerText : "";
+        return parent.innerText ? applyTransformations(parent.innerText, patterns) : "";
       },
-      () => element.getAttribute("text"),
+      () => element.getAttribute("text") ? applyTransformations(element.getAttribute("text"), patterns) : "",
       () => element.src,
       () => {
         const img = element.querySelector("img[src]");
         return img ? img.src : "";
       },
-
       () => element.className,
     ];
 
@@ -41,6 +53,8 @@
       if (value) break;
     }
 
+    value.trim();
+
     // Check for specific SVGs and append label
     if (svgCloseCheck) {
       value += " 닫기";
@@ -48,10 +62,15 @@
       value += " 이동하기";
     }
 
+    var elementTitle = applyTransformations(element.title, patterns);
+    if(elementTitle) {
+      value += '>'+ elementTitle;
+    }
+
     // Trim the value to 100 characters if necessary
     return value.length > 100 ? value.substring(0, 100) : value;
   }
-
+  
   // Select elements based on given conditions and add data-gtm-auto-click attribute
   const clickSelectors = [
     // 조건 1: button 태그 주변에 data-gtm-click 또는 data-gtm-auto-click이 없고, 하위에 button 또는 a 태그가 없는 경우
@@ -64,12 +83,11 @@
     "li:not([data-gtm-click]):not([data-gtm-auto-click]):not(:has(button, a))",
     
     // 조건 4: .swiper-slide 클래스가 있는 태그에 data-gtm-click 또는 data-gtm-auto-click이 없는 경우
-    ".swiper-slide:not([data-gtm-click]):not([data-gtm-auto-click])",
+    ".swiper-slide:not([data-gtm-click]):not([data-gtm-auto-click]):not(:has(button, a))",
     
     // 조건 5: input 태그에 data-gtm-click 또는 data-gtm-auto-click이 없고, 부모 또는 자식 요소에 button 또는 a 태그가 없는 경우
     "input:not([data-gtm-click]):not([data-gtm-auto-click]):not(:has(button, a)):not(:has(button, a))",
 
-    ".collapse:not([data-gtm-click]):not([data-gtm-auto-click])"
   ];
   document.querySelectorAll(clickSelectors.join(",")).forEach((element) => {
     if (!hasClosestWithPrefix(element, "data-gtm-click")) {
